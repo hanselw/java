913d11d52ac1e2a93eee21b6c92dd7a4a93e5606
hanselwang <hansel_wang@outlook.com>
2017-11-24 17:39:52 +0800

    mycode

---
+++ mycode/src/jdktest/JdkTest.java
@@ -0,0 +1,22 @@
+/**
+ * @author wanghansong
+ * No:
+ * Details:
+ * 2017-11-24обнГ05:39:04
+ */
+package jdktest;
+
+public class JdkTest {
+
+	/**
+	 * wanghansong
+	 * 2017-11-24обнГ05:39:04
+	 * @param args
+	 *
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
