57227c1404e23a94320484a703ece705eb1b5d6c
hanselwang <hansel_wang@outlook.com>
2017-11-29 17:34:43 +0800

    算法lib以及代码

--- mycode/src/jdktest/JdkTest.java
+++ mycode/src/jdktest/JdkTest.java
@@ -6,6 +6,9 @@
  */
 package jdktest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JdkTest {
 
 	/**
@@ -16,7 +19,21 @@ public class JdkTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//testMap();
+		testDataType();
 	}
 
+	public static void testMap(){
+		Map<String, String> d = new HashMap<String, String>();
+		for(int i = 100; i < 200; i++){
+			d.put(i+"", i+"");
+		}
+		System.out.println(d.size());
+	}
+	
+	public static void testDataType(){
+		System.out.println(Math.abs(-2147483648));
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Math.pow(2, 31));
+	}
 }
